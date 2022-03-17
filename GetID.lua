51d7b6f5120820e33cbffce292489bc2ad913e4f
kimware87 <101164854+kimware87@users.noreply.github.com>
2022-03-17 19:44:33 +0800

    Add files via upload
    Lua file for getting Device ID of Keyboards

---
+++ GetID.lua
@@ -0,0 +1,6 @@
+-- Get Keyboard ID
+lmc_assign_keyboard('MACROS')
+lmc_print_devices()
+
+-- lmc_assign_keyboard('MACROS') - Will set the name MACROS to the 2nd Keyboard you Pressed
+-- lmc_print_devices() - Will list all list of Keyboard and other devices, look for the named device to get the DeviceID
